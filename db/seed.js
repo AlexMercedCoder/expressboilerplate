84ab4a1272f19a739cf1b3634b7b66104a36f6e8
Alex Merced <alex@alexmerced.dev>
2021-03-11 11:46:00 -0500

    first commit

---
+++ db/seed.js
@@ -0,0 +1,25 @@
+//Import The Database Connection
+const mongoose = require("./connection")
+
+///////////////////////////////////////////
+// IMPORT YOUR MODELS BELOW
+///////////////////////////////////////////
+
+
+///////////////////////////////////////////
+// DO YOUR DATABASE OPERATIONS IN BELOW FUNCTION
+///////////////////////////////////////////
+
+const seed = async () => {
+
+    //*********Code Goes Here
+
+
+
+    //***************************** */
+
+    mongoose.disconnect()
+}
+
+// Run Seed Function
+seed()
